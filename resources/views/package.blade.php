f7e6fa864646651b856b6b5f28685e3a8945f87f
nguyendinhkhoa19t1@gmail.com <nguyendinhkhoa19t1@gmail.com>
2022-01-09 00:05:06 +0700

    update package page

--- resources/views/package.blade.php
+++ resources/views/package.blade.php
@@ -56,8 +56,7 @@
         </div>
     </div>
 
-    <section class="ftco-section ftco-services" style="overflow: hidden">
-        <h1 style="text-align: center; margin-bottom: 50px; color: rgb(112, 112, 112)">Đặt ngay</h1>
+    <section class="ftco-section ftco-services">
         <div class="row">
             <div class="col-md-8">
                 <h1 style="text-align: center">Our Menu</h1>
@@ -128,22 +127,25 @@
                 <h1 style="text-align: center; margin-bottom: 50px; color: rgb(112, 112, 112)">Đặt ngay</h1>
                 <form method="POST" action="{{ route('createOrder') }}">
                     @csrf
-                    <div class="form-order">
-                        <div class="form-group col-3">
+                    <div class="form-row">
+                        <div class="form-group row">
                             <label for="organizationDate"
                                 style="font-size: 1.2em; color: rgb(112, 109, 109); font-weight: 400">Thời
                                 gian</label>
                             <input type="datetime-local" class="inp form-control" id="dt" placeholder="Ngày/giờ"
                                 name="organizationDate" style="color: rgb(105, 105, 105)">
                         </div>
-                        <div class="form-group col-2 ">
+                        <div class="form-group row my-col">
                             <label style="font-size: 1.2em; color: rgb(112, 112, 112); font-weight: 400"
                                 for="peopleNumber">Số
                                 lượng
                                 người</label>
                             <input type="number" class="inp form-control" id="peopleNumber" name="peopleNumber">
                         </div>
-                            <select name="menuId" id="menuId">
+                        <div class="form-group row my-col sel">
+                            <label style="font-size: 1.2em; color: rgb(133, 128, 128); font-weight: 400"
+                                for="menuId">Menu</label>
+                            <select class="form-control sel menu-select" style="width: 200%" name="menuId">
                                 <option value="0">Chọn menu</option>
                                 @foreach ($menus as $menu)
                                     <option value="{{ $menu->id }}">{{ $menu->name }}</option>
@@ -153,20 +155,17 @@
                         <div class="form-group row my-col" style="width: 120%">
                             <label style="font-size: 1.2em; color: rgb(121, 118, 118); font-weight: 400" for="note">Ghi
                                 chú</label>
-                            <input type="text" class=" form-control" id="note" name="note"></input>
-                        <div class="form-group col-3 btn-order d-flex align-items-end">
-                            <button style="width: 100%;border-color: rgb(126, 125, 125)" class="btn btn-success"
+                            <textarea class="inp form-control" id="note" name="note"></textarea>
+                        </div>
+                        <div class="row btn-order">
+                            <button style="width: 100%;border-color: rgb(126, 125, 125)" class="btn btn-dark"
                                 type="submit">Đặt
                                 đơn</button>
                         </div>
                     </div>
 
                 </form>
-    
-                
             </div>
-
-            
         </div>
 
         <div class="modal fade" id="modalChooseFood" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
@@ -420,31 +419,4 @@
     </script>
 @endsection
 
-@endsection
-
-@section('js')
-    <script>
-        $(document).ready(function() {
-            $('#menu').on('change', function(event) {
-                event.preventDefault();
-                var id = $('#menu option').filter(":selected").val();
-                // var id = $('#menu').value;
-                // alert(id);
-                var _token = $('input[name=_token]').val(); 
-                $.ajax({
-                    url:"{{route('showMenuById')}}",
-                    method:"POST",
-                    data:{
-                        id: id,
-                        _token: _token
-                    },
-                    dataType:"JSON",
-                    success:function(data){
-                        $('#table-select-food tbody').html(data.table_data);
-                    }
-                })
-            })
-        })
-
-    </script>
 @endsection
